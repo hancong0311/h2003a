8ee44cd3a5f5a52c0dcca6b3461193a82888e088
hancong <hancong@163.com>
2021-01-20 14:56:55 +0800

    项目初始化行为

---
+++ src/main/java/com/test/Test.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+        * @Title: Test
+        * @Description:
+        * @Auther:
+        * @Version: 1.0
+        * @create 2021/1/20 14:50
+        */
+public class Test {
+}
